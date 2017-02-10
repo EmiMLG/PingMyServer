93e446a87a7b3ca6916b86b967e9b8923ccfadb1
Emi <emimlg1@gmail.com>
2017-02-10 11:20:25 +0100

    added alarm service

--- app/src/main/java/es/telefonica/talentum/pingmyserver/activities/MainActivity.java
+++ app/src/main/java/es/telefonica/talentum/pingmyserver/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package es.telefonica.talentum.pingmyserver.activities;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -31,10 +34,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(view.getContext(),PingService.class);
-
                 i.putExtra("texto", editText.getText().toString());
 
-                startService(i);
+                PendingIntent pendingIntent = PendingIntent.getService(view.getContext(),69,i,0); //El pendingIntent se queda guardado
+
+                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME, //Ejecuta desde el momento actual
+                        SystemClock.elapsedRealtime(),//Dame el tiempo actual del telefono en ms
+                        5 * 1000, //tiempo en milisegundos
+                        pendingIntent
+                );
+
+                // startService(i);
             }
         });
 
