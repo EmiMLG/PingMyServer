a0f6641cfbb3f5e57f062054182a342fa24394da
Emi <emimlg1@gmail.com>
2017-02-10 10:21:49 +0100

    added onCreate and Destroyed

--- app/src/main/java/es/telefonica/talentum/pingmyserver/activities/servicies/PingService.java
+++ app/src/main/java/es/telefonica/talentum/pingmyserver/activities/servicies/PingService.java
@@ -2,14 +2,18 @@ package es.telefonica.talentum.pingmyserver.activities.servicies;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.util.Log;
-import android.widget.EditText;
 
 
 public class PingService extends IntentService { //TODO 1: Todos los servicios extienden de IntentService
 
-    EditText editText;
+    @Override
+    public void onCreate() {  //Esto se ejecutará primero
+        super.onCreate();
+        Log.d("PingService", "It's Alive");
 
+    }
     public PingService(){
             this("PingService");
         }
@@ -23,19 +27,13 @@ public class PingService extends IntentService { //TODO 1: Todos los servicios e
         String s = intent.getStringExtra("texto");
         Log.d("PingService", "Hello World" + s);
 
+        SystemClock.sleep(3000);
+        Log.d("PingService", "Tarea finalizada");
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Log.d("PingService", "It's Alive");
-
-    }
 
     @Override
-    public void onDestroy() {
-
+    public void onDestroy() { //Se ejecutará cuando termine el onHandleIntent
         Log.d("PingService", "Hello World" + "I'm dieee");
-
     }
 }
