10901552e3402381206477124ac952508da10a82
Emi <emimlg1@gmail.com>
2017-02-10 09:59:27 +0100

    added edittext

--- app/src/main/java/es/telefonica/talentum/pingmyserver/activities/MainActivity.java
+++ app/src/main/java/es/telefonica/talentum/pingmyserver/activities/MainActivity.java
@@ -1,15 +1,45 @@
 package es.telefonica.talentum.pingmyserver.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import es.telefonica.talentum.pingmyserver.R;
+import es.telefonica.talentum.pingmyserver.activities.servicies.PingService;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button launchServiceButton;
+    EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        launchServiceButton = (Button) findViewById(R.id.activity_main___launch_button);
+        editText = (EditText) findViewById(R.id.activity_main___edit_text);
+
+
+        launchServiceButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(),PingService.class);
+
+                i.putExtra("texto", editText.getText().toString());
+
+                startService(i);
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/es/telefonica/talentum/pingmyserver/activities/servicies/PingService.java
+++ app/src/main/java/es/telefonica/talentum/pingmyserver/activities/servicies/PingService.java
@@ -1,4 +1,41 @@
 package es.telefonica.talentum.pingmyserver.activities.servicies;
 
-public class PingService {
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.EditText;
+
+
+public class PingService extends IntentService { //TODO 1: Todos los servicios extienden de IntentService
+
+    EditText editText;
+
+    public PingService(){
+            this("PingService");
+        }
+
+    public PingService(String name) { //TODO 3: De nuevo alt intro para crear el constructor
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) { //TODO 2: Implementamos el método con alt intro
+        String s = intent.getStringExtra("texto");
+        Log.d("PingService", "Hello World" + s);
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("PingService", "It's Alive");
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+        Log.d("PingService", "Hello World" + "I'm dieee");
+
+    }
 }
